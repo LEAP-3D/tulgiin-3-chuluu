e7d64b14ffdc0df6c5dd572fbc6736638f048cf8
TOM-BOSS-pinetour <b94889340@gmail.com>
2026-01-28 12:12:44 +0800

    Test

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactCompiler: true,
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL || "",
+  },
+};
+
+export default nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { useEffect } from "react";
+
+const Home = () => {
+  return <>hello fssrom sdaheome</>;
+};
+
+export default Home;
