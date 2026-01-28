676420265ad95cedc6bd41f75a168b7a09183b51
TOM-BOSS-pinetour <b94889340@gmail.com>
2026-01-28 12:05:25 +0800

    React Native

---
+++ .next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"AnUullKu1IND0BK6Ie5NWmbOSYkq+qDjYxcLqGtDFkc=\"\n}"
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useEffect } from "react";
-
-const Home = () => {
-  return <>hello fssrom sdaheome</>;
-};
-
-export default Home;
